4a536709cc87e9e5cfd3a7af8deb04e31d03abaf
AyushGupta0202 <ayushg430@gmail.com>
2024-05-19 09:07:44 +0530

    added __init__.py

--- CommandHelper.py
+++ CommandHelper.py
@@ -2,11 +2,8 @@ import subprocess
 import click
 
 def executeSubprocessCommand(command):
-    status = subprocess.getstatusoutput(command)
-    if (status[0] == 0):
-        return status[1]
-    click.echo(status[1])
-    return "-1"
+    return subprocess.getstatusoutput(command)
 
 def executeCommandAndEchoOutput(command):
     status = executeSubprocessCommand(command)
+    click.echo(status)
